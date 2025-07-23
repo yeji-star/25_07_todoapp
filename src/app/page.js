bab623ee7923865cdb65b5f390b1cfeab4cbe528
yeji-star <sizuku134@gmail.com>
2025-07-23 13:18:51 +0900

    정리

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,23 @@
 import Image from "next/image";
 
 export default function Home() {
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 
-  
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+
          안녕
-          
         </div>
+
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-    
-      </footer>
+
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center" />
+
     </div>
+
   );
+  
 }
