e931bc418d89abaf03e8387aa37a3ff7a8e45c42
yeji-star <sizuku134@gmail.com>
2025-07-24 12:19:37 +0900

    tailwind.config 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
+'use client';
+
 import Image from 'next/image';
+import Button from '@mui/material/Button';
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <button>버튼</button>
+      <Button className="bg-red-500" variant="contained">
+        Contained
+      </Button>
+    </>
+  );
 }
